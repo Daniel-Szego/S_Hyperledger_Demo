40ef99bef8ca5a0b310ccadcdf6df2447f6a2325
Daniel-Szego <szego.daniel@gmail.com>
2019-01-14 09:59:40 +0100

    delete function for data (DO NOT USE IN PRODUCTION)

--- js/sequenceScript.js
+++ js/sequenceScript.js
@@ -81,21 +81,77 @@ async function RetireTransaction(tx) {  // eslint-disable-line no-unused-vars
 
 
 /**
- * Retire transaction.
+ * DELETE THIS FUNCTION IN PRODUCTION
+ * Creating test - demo data for the application.
  * @param {org.sequence.model.CreateTestData} tx The sample transaction instance.
  * @transaction
  */
 async function CreateTestDataTransaction(tx) {  // eslint-disable-line no-unused-vars
   
+    console.log('create test data'); 
+    const factory = getFactory(); 
+
+  
+    console.log('Creating a Key 1');  
+	
+  	// adding key 1 
+    const keyReg = await getParticipantRegistry(namespace + '.Keys');   
+    const key1 = await factory.newResource(namespace, 'Keys', "1");
+    await keyReg.add(key1);       
+
+    // adding key 2 
+    const key2 = await factory.newResource(namespace, 'Keys', "2");
+    await keyReg.add(key2);       
+
+    // adding key 3
+    const key3 = await factory.newResource(namespace, 'Keys', "3"); 
+    await keyReg.add(key3);       
+
+  
+  
 }
 
 /**
- * Retire transaction.
+ * DELETE THIS FUNCTION IN PRODUCTION
+ * Deleting all data.
  * @param {org.sequence.model.DeleteAllData} tx The sample transaction instance.
  * @transaction
  */
 async function DeleteAllDataTransaction(tx) {  // eslint-disable-line no-unused-vars
+    console.log('clearing test data');
 
+    // deleting assets -  keys
+    const keysRegistry = await getAssetRegistry(namespace + '.Keys'); 
+    let keys = await keysRegistry.getAll();
+    await keysRegistry.removeAll(keys);
+  
+    // deleting assets -  Flavour
+    const flavourRegistry = await getAssetRegistry(namespace + '.Flavour'); 
+    let flavour = await flavourRegistry.getAll();
+    await flavourRegistry.removeAll(flavour);
+
+    // deleting assets -  Tokens
+    const tokenRegistry = await getAssetRegistry(namespace + '.Tokens'); 
+    let tokens = await tokenRegistry.getAll();
+    await tokenRegistry.removeAll(tokens);
+  
+    // deleting assets -  Actions
+    const actionsRegistry = await getAssetRegistry(namespace + '.Actions'); 
+    let actions = await actionsRegistry.getAll();
+    await actionsRegistry.removeAll(actions);
+  
+    // deleting assets -  Transactions
+    const transactionsRegistry = await getAssetRegistry(namespace + '.Transactions'); 
+    let transactions = await transactionsRegistry.getAll();
+    await transactionsRegistry.removeAll(transactions);
+ 
+  	// deleting participants
+    const accountRegistry = await getParticipantRegistry(namespace + '.Account');
+    let accounts = await accountRegistry.getAll();
+    await accountRegistry.removeAll(accounts);
+      
+    console.log('clearing all data finished');  
   
 }
+
 
